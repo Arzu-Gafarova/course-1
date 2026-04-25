5bc1048f782d430dc65afec6f3aaf57b391790ad
Arzu-Gafarova <arzugafarova489@mail.ru>
2026-04-25 17:09:33 +0300

    Add files via upload

---
+++ projects/ChatBot/Commands/IBotCommand.cs
@@ -0,0 +1,10 @@
+﻿using ChatBot.Dtos;
+using Telegram.Bot;
+
+public interface IBotCommand
+{
+    string Trigger { get; }
+    Task ExecuteAsync(TelegramUpdate update, ITelegramBotClient bot, long chatId);
+
+}
+
---
+++ projects/ChatBot/Commands/QuoteCommand.cs
@@ -0,0 +1,37 @@
+﻿using ChatBot.Dtos;
+using ChatBot.Repositories.Interfaces;
+using ChatBot.Repositories.Models;
+using Telegram.Bot;
+
+namespace ChatBot.Commands
+{
+    public class QuoteCommand : IBotCommand
+    {
+        private readonly IChatApiClient _chatApiClient;
+
+        public QuoteCommand(IChatApiClient chatApiClient)
+        {
+            _chatApiClient = chatApiClient;
+        }
+
+        public string Trigger => "/quote";
+
+        public async Task ExecuteAsync(TelegramUpdate update, ITelegramBotClient bot, long chatId)
+        {
+            
+            var emptyHistory = new List<OpenApiResponse.Message>();
+
+            var prompt = "Отправь цитату, котороую я сегодня точно должен услышать.";
+
+            try
+            {
+                var quote = await _chatApiClient.SendMessageAsync(prompt, emptyHistory);
+                await bot.SendTextMessageAsync(chatId, $"*Цитата которую вы точно должны сегодня услышать:*\n\n{quote}", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            }
+            catch
+            {
+                await bot.SendTextMessageAsync(chatId, "Не удалось получить цитату");
+            }
+        }
+    }
+}
